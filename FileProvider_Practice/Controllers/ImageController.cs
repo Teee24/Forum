fd11ecb690df7d37029b4777b076282377d2459a
tinalien <tinalien0728@gmail.com>
2024-05-17 14:52:20 +0800

    feat : 新增GetImage
    -用FileProvider去建立

---
+++ FileProvider_Practice/Controllers/ImageController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using System.Text;
+using System;
+
+namespace FileProvider_Practice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly IFileProvider _fileProvider;
+        public ImageController(IFileProvider fileProvider)
+        {
+            this._fileProvider = fileProvider;
+        }
+      
+
+        [HttpGet]
+        public IActionResult GetImage(int id)
+        {
+            IFileInfo fileInfo = _fileProvider.GetFileInfo(id.ToString() + ".jpg");
+
+            if (fileInfo.Exists)
+            {
+                return File(fileInfo.CreateReadStream(), "image/jpeg");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
