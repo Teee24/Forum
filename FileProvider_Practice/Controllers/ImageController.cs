1ae94d79f735e36ff56a2a9db67bc6eee26cb2a6
tinalien <tinalien0728@gmail.com>
2024-05-17 16:03:01 +0800

    feat : 新增UplaodImage
    -新增的圖片以數字命名加入Image資料夾中

--- FileProvider_Practice/Controllers/ImageController.cs
+++ FileProvider_Practice/Controllers/ImageController.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Text;
 using System;
 
+
 namespace FileProvider_Practice.Controllers
 {
     [Route("api/[controller]")]
@@ -12,9 +13,11 @@ namespace FileProvider_Practice.Controllers
     public class ImageController : ControllerBase
     {
         private readonly IFileProvider _fileProvider;
-        public ImageController(IFileProvider fileProvider)
+        private readonly IWebHostEnvironment _environment;
+        public ImageController(IFileProvider fileProvider,IWebHostEnvironment environment)
         {
             this._fileProvider = fileProvider;
+            _environment = environment;
         }
       
 
@@ -31,6 +34,30 @@ namespace FileProvider_Practice.Controllers
             {
                 return NotFound();
             }
+        }
+
+        [HttpPost] 
+        public IResult UploadImage(IFormFile file)
+        {
+            
+            string path = Path.Combine(_environment.WebRootPath + "Image");
+
+            //確認有沒有Image料夾
+            if (!Directory.Exists(path))
+            {
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                Console.WriteLine("建立資料夾");
+            }
+            //以目前圖片數量+1命名
+            int lastfileName = Directory.GetFiles(path, "*.jpg").Length;
+            string filename = (lastfileName + 1).ToString() + ".jpg";
+            //file to byte[]
+
+            using (var stream = System.IO.File.Create(Path.Combine(path, filename)))
+            {
+                file.CopyToAsync(stream);
+            }
+            return Results.Ok(lastfileName);
         }
 
     }
--- FileProvider_Practice/Program.cs
+++ FileProvider_Practice/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Directory.GetCurrentDirectory()+"\\Image"));
 
 var app = builder.Build();
